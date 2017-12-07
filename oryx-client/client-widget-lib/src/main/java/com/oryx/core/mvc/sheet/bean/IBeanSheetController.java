f4e4546fa42b5905bf1d631b0badb74f17ee0666
SAMI <sami.mbarki@gmail.com>
2017-12-07 21:02:30 +0100

    171207.2042

---
+++ oryx-client/client-widget-lib/src/main/java/com/oryx/core/mvc/sheet/bean/IBeanSheetController.java
@@ -0,0 +1,10 @@
+package com.oryx.core.mvc.sheet.bean;
+
+import com.oryx.core.mvc.sheet.ISheetController;
+import com.oryx.core.mvc.view.IViewController;
+
+/**
+ * Created by smbarki on 06/12/2017.
+ */
+public interface IBeanSheetController<E> extends ISheetController<E> {
+}
